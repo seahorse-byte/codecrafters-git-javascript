bb928d8fc763d040ec85b3931e67a8e2b881926d
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-11-17 23:01:46 +0000

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 console.error("Logs from your program will appear here!");
 
@@ -8,6 +9,9 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    catFile();
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -25,4 +29,19 @@ function createGitDirectory() {
     "ref: refs/heads/main\n"
   );
   console.log("Initialized git directory");
+}
+
+function catFile() {
+  const hash = process.argv[4];
+  const dir = hash.substring(0, 2);
+  const file = hash.substring(2);
+
+  const objectPath = path.join(process.cwd(), ".git", "objects", dir, file);
+  const blob = fs.readFileSync(objectPath);
+  const bufferToString = zlib.unzipSync(blob).toString();
+
+  // Extract content after null byte separator
+  const nullByteIndex = bufferToString.indexOf("\x00");
+  const content = bufferToString.substring(nullByteIndex + 1);
+  process.stdout.write(content);
 }
